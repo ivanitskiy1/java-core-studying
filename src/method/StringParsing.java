e8208d409d19786f41ae7cb8b1a93f5bdde6281c
ivanitskiy1 <ivanitskiy182@gmail.com>
2018-02-17 19:30:17 +0200

    calculateCharQuantity

---
+++ src/method/StringParsing.java
@@ -0,0 +1,21 @@
+package method;
+
+public class StringParsing {
+    public static void main(String[] args) {
+        String string = "Ivanitskii Pavel";
+        printChars(string);
+        int quantity = calculateCharsQuantity(string, 'i');
+        System.out.println("Char quantity: " + quantity);
+    }
+
+    public static void printChars(String string) {
+        char[] chars = string.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            System.out.println(chars[i]);
+        }
+    }
+
+    public static int calculateCharsQuantity(String string, char condition) {
+        return 3;
+    }
+}
