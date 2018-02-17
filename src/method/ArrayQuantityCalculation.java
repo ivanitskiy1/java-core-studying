88ac9178cf9b2c37631724270381e3e0a804d9c8
ivanitskiy1 <ivanitskiy182@gmail.com>
2018-02-17 22:09:22 +0200

    calculateCharQuantity 2

---
+++ src/method/ArrayQuantityCalculation.java
@@ -0,0 +1,12 @@
+package method;
+
+public class ArrayQuantityCalculation {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int calculateElementsQuantity(int[] array, int condition) {
+        return 9;
+    }
+}
